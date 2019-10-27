576d7796364502490f588cd11c0a6566e25b7c0d
Zach Krogh <zakrogh@gmail.com>
2019-10-27 15:49:12 -0700

    setup model structure

---
+++ PierresTreats/Models/Flavor.cs
@@ -0,0 +1,18 @@
+using System.Collections.Generic;
+
+namespace PierresTreats.Models
+{
+    public class Flavor
+    {
+        public Flavor()
+        {
+            this.Treats = new HashSet<TreatFlavor>();
+        }
+
+        public int FlavorId { get; set; }
+        public string Name { get; set; }
+        public virtual ApplicationUser User { get; set; }
+
+        public ICollection<TreatFlavor> Treats { get;}
+    }
+}
